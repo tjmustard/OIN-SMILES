4134561d4c3012b302bc6b9db0831b42465a53a9
Thomas Mustard <tjmustard@mustardfamily.com>
2026-07-11 11:10:18 -0700

    Remove rebuild.sh

--- tmCAT-tmPHOTO_xyz_dataset/rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Rebuild the summary_roundtrip.json file from the individual reports.
-
-cd "$(dirname "$0")"/.. || exit 1
-uv run python tools/rebuild_summary.py --output-dir tmCAT-tmPHOTO_xyz_dataset/20260707-results
